bcea7abc701e32079fc550bcec5569eb59e5f344
Chu wei <kyoii377@gmail.com>
2024-07-26 08:39:23 +0800

    Adjust some code

--- Netflix Clone/Netflix Clone/Extension/UIViewController+.swift
+++ Netflix Clone/Netflix Clone/Extension/UIViewController+.swift
@@ -18,20 +18,18 @@ extension UIViewController {
         return statusBarHeight
     }
     
-    var topbarHeight: CGFloat {
+    var toScreenTopHeight: CGFloat {
         return statusBarHeight  + (navigationController?.navigationBar.frame.height ?? 0.0)
     }
     
-    /// Safe area top padding
-    var topPadding: CGFloat {
+    var safeAreaTopPadding: CGFloat {
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
               let mainWindow = windowScene.windows.first else { return 0 }
         let topPadding = mainWindow.safeAreaInsets.top
         return topPadding
     }
     
-    /// Safe area bottom padding
-    var bottomPadding: CGFloat {
+    var safeAreaBottomPadding: CGFloat {
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
               let mainWindow = windowScene.windows.first else { return 0 }
         let bottomPadding = mainWindow.safeAreaInsets.bottom
@@ -39,27 +37,6 @@ extension UIViewController {
     }
     
     // MARK: - Methods
-    
-    func setupNavigationStyle(title: String, preferLargeTitle: Bool, background: UIColor) {
-        // Title
-        navigationItem.title = title
-        navigationController?.navigationBar.prefersLargeTitles = preferLargeTitle
-        navigationController?.navigationItem.largeTitleDisplayMode = .always
-
-        // Navigation Background Color
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        appearance.backgroundColor = background
-        
-        // NavigationBar
-        navigationController?.navigationBar.tintColor = .white
-        navigationController?.navigationBar.standardAppearance = appearance
-        navigationController?.navigationBar.compactAppearance = appearance
-        navigationController?.navigationBar.scrollEdgeAppearance = appearance
-        navigationController?.navigationBar.isTranslucent = true
-        navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
-    }
     
     func bridgeSwiftUIView(_ swiftUIView: some View) {
         let hostingController = UIHostingController(rootView: swiftUIView)
